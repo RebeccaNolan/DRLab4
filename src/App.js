4877cf2a360cf808d95298e8a0bea76ecc88efa6
Rebecca Nolan <G00423921@atu.ie>
2024-10-04 11:02:56 +0000

    Ex 5 completed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import './App.css';
 import Content from './Content';
 import Header from './Header';
 import Footer from './Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './Navbar';
 
 function App() {
   return (
     //Ex 2 - Add div to display hello world and date
     <div>
+      <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<h1>Read Component</h1>} />
+        <Route path="/create" element={<h1>Create Component</h1>} />
+      </Routes>
+      <Footer />
+    </Router>
+    
       <Content></Content>
       <Header></Header>
       <Footer></Footer>
