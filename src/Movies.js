00a1b834067220c5c9cc98e880abef07dc7647a7
Rebecca Nolan <127899503+RebeccaNolan@users.noreply.github.com>
2024-10-13 14:02:06 +0100

    Comments Movies.js

--- src/Movies.js
+++ src/Movies.js
@@ -1,12 +1,12 @@
 
-import MovieItem from "./MovieItem";
+import MovieItem from "./MovieItem"; //import MovieItem component
 
 const Movies = (props)=> { /*props passes data from parent to child*/
-    return props.movieDisp.map(
+    return props.movieDisp.map( /*Maps over movieDisp array (passed via props)*/
         (movie)=> {
-            return <MovieItem myMovie = {movie} key={movie.imdbID}/> 
+            return <MovieItem myMovie = {movie} key={movie.imdbID}/>  //Pass each movie object to MovieItem, using imdbID as key
         }
     );
 }
 
-export default Movies;
+export default Movies; //export movies component
