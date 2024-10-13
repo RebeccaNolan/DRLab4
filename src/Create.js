71490ec43b78cbf3e2c8de79367d467910ccb30d
Rebecca Nolan <127899503+RebeccaNolan@users.noreply.github.com>
2024-10-13 13:57:53 +0100

    Comments Create.js

--- src/Create.js
+++ src/Create.js
@@ -1,5 +1,7 @@
-const Create = () => {
+//Create component function
+//h1 indicating create page
+const Create = () => { 
     return <h1>This is a create</h1>;
   };
   
-  export default Create;
+  export default Create; //Export create component
